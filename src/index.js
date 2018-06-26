36b9e17d57f3c3d1c2a6c711b5124bb08a47a82b
David Tsai <tsaiDavid@users.noreply.github.com>
2018-06-26 08:33:37 -0700

    Add iframe as child to parent

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 document.getElementById("app").innerHTML = `
-<h1>Hello Parcel!</h1>
-<div>
-  Look
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about Parcel.
-</div>
+  <div>
+    <div id="child-data-disp"></div>
+  </div>
+  <br>
+  <div>
+    <iframe id="frame" src="./src/iframe.html" />
+  </div>
 `;
+const iframeEl = document.getElementById("frame");
+console.log(iframeEl);
